085521b279f2f1955ec85e589bacffd6777a9fc9
Serphis <serphis48@wp.pl>
2024-12-11 23:07:28 +0100

    zmiana dir createorder

--- app/routes/checkout.tsx
+++ app/routes/checkout.tsx
@@ -111,7 +111,7 @@ export default function Checkout() {
     };
 
     try {
-      const response = await fetch('/api/createOrder', {
+      const response = await fetch('createOrder', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ orderData }),
